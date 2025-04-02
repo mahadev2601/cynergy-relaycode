729e3cc8f6ff48d7ec241409e00da2df9af1d52c
mahadev2601 <mahadevlakshmanan@gmail.com>
2025-04-02 01:50:31 -0700

    Create coders_problem2(2).py

---
+++ coders_problem2(2).py
@@ -0,0 +1,69 @@
+#mahadeva
+a=input("Enter a roman letter within 10:")
+b=input("Enter First roman letter within 10:")
+
+if (a=="i"):
+    a=1
+elif(a=="ii"):
+    a=2
+elif(a=="iii"):
+    a=3
+elif(a=="iv"):
+    a=4
+elif(a=="v"):
+    a=5
+elif(a=="vi"):
+    a=6
+elif(a=="vii"):
+    a=7
+elif(a=="viii"):
+    a=8
+elif(a=="ix"):
+    a=9
+elif(a=="x"):
+    a=10
+
+if (b=="i"):
+    b=1
+elif(b=="ii"):
+    b=2
+elif(b=="iii"):
+    b=3
+elif(b=="iv"):
+    b=4
+elif(b=="v"):
+    b=5
+elif(b=="vi"):
+    b=6
+elif(b=="vii"):
+    b=7
+elif(b=="viii"):
+    b=8
+elif(b=="ix"):
+    b=9
+elif(b=="x"):
+    b=10
+c = print("sum of the roman numbers: ",a + b )
+
+if (c==1):
+    c='i'
+elif(c==2):
+    c='ii'
+elif(c==3):
+    c="iii"
+elif(c==4):
+    c="iv"
+elif(c==5):
+    c="v"
+elif(c==6):
+    c="vi"
+elif(c==7):
+    c="vii"
+elif(c==8):
+    c="viii"
+elif(c==9):
+    c="xi"
+elif(c==10):
+    c="x"
+    print(c)
+
