9ece88ad281926e9c78319938b01fefa4ad19bb7
mahadev2601 <mahadevlakshmanan@gmail.com>
2025-04-02 01:07:28 -0700

    Mahadeva

---
+++ coders_problem2.py
@@ -0,0 +1,53 @@
+#mahadev
+a=str(input("Enter a roman letter within 10:"))
+b=str(input("Enter First roman letter within 10:"))
+
+if (a=="i"):
+    a=1
+
+elif(a=="ii"):
+    a=2
+
+elif(a=="iii"):
+    a=3
+
+elif(a=="iv"):
+    a=4
+elif(a=="v"):
+    a=5
+elif(a=="vi"):
+    a=6
+elif(a=="vii"):
+    a=7
+elif(a=="viii"):
+    a=8
+elif(a=="ix"):
+    a=9
+elif(a=="x"):
+    a=10
+
+elif(b=="i"):
+    b=1
+
+elif(b=="ii"):
+    b=2
+
+elif(b=="iii"):
+    b=3
+
+elif(b=="iv"):
+    b=4
+elif(b=="v"):
+    b=5
+elif(b=="vi"):
+    b=6
+elif(b=="vii"):
+    b=7
+elif(b=="viii"):
+    b=8
+elif(b=="ix"):
+    b=9
+elif(b=="x"):
+    b=10
+
+print("sum of the roman numbers: ")
